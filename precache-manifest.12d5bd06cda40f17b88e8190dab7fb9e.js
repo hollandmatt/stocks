8d9d0bc5cbd2fcca7881bb6afd3e33b72238b4a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-07 23:33:38 +0000

    Deploy hollandmatt/stocks to github.com/hollandmatt/stocks.git:gh-pages

---
+++ precache-manifest.12d5bd06cda40f17b88e8190dab7fb9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40ce45ec8fa9a77542a2",
+    "url": "/stocks/static/css/main.6896b7a7.chunk.css"
+  },
+  {
+    "revision": "40ce45ec8fa9a77542a2",
+    "url": "/stocks/static/js/main.5901563d.chunk.js"
+  },
+  {
+    "revision": "bf38fc93852ff617158a",
+    "url": "/stocks/static/js/runtime~main.9551dd32.js"
+  },
+  {
+    "revision": "500ed764ac615d50bd1f",
+    "url": "/stocks/static/js/2.d424301a.chunk.js"
+  },
+  {
+    "revision": "cd8c57655e8b9d07ef9804b01c037f54",
+    "url": "/stocks/index.html"
+  }
+];
