84e642087d3dce34f9c3ded22ec22285cbdbb25d
Matt Holland <hollandmatt@gmail.com>
2019-04-07 14:16:33 -0700

    sort the options in the dropdown

--- src/components/StocksList.js
+++ src/components/StocksList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import _ from 'lodash'
 import Select from 'react-select';
 
 const StocksList = ({ stocks, onChange }) => {
-  const options = stocks.map(stock => {
+  const options = _.sortBy(stocks.map(stock => {
     return {
       value: stock.symbol,
       label: stock.companyName
     };
-  });
+  }), o => o.label);
   return <Select options={options} onChange={onChange} />;
 };
 
